72e16bd35776cd19b2b91b727a6e543b5d5f8a47
Pic <nik20010106@gmail.com>
2024-12-25 00:56:58 +0200

    Hw_018

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,1 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class Main {
-    public static void main(String[] args) {
-            UI ui= new UI();
-            ui.start();
-
-
-    }
-}
+/.
